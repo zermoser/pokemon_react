c20f70b44d09c257c4ecd24f02ae6b80b6386466
zermoser <kittipoj.na@gmail.com>
2024-06-30 21:16:34 +0700

    Complete Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Detail from './components/Detail';
 import Pocket from './components/Pocket';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
@@ -12,11 +13,12 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <div className="flex flex-col min-h-screen">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/detail/:id" element={<Detail />} />
-              <Route path="/pocket" element={<Pocket />} />
-            </Routes>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/pocket" element={<Pocket />} />
+          </Routes>
           <Footer />
         </div>
       </Router>
